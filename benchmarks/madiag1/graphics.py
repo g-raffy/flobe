8ef0a941eb089cacd4340ebf6cc530f644727ed5
Guillaume Raffy <guillaume.raffy@univ-rennes1.fr>
2024-07-13 23:51:51 +0200

    added a graph showing the speedup of gpu realtive to the cpu
    work related to [https://bugzilla.ipr.univ-rennes.fr/show_bug.cgi?id=3886]

--- benchmarks/madiag1/graphics.py
+++ benchmarks/madiag1/graphics.py
@@ -51,7 +51,12 @@ class SvgFigureHandler(IFigureHandler):
         self._out_svg_dir_path = out_svg_dir_path
 
     def on_figure_ended(self, fig):
-        fig.savefig(self._out_svg_dir_path + '/' + fig.axes[0].get_title() + '.svg')
+        file_name = None
+        if hasattr(fig, 'file_name'):
+            file_name = fig.file_name
+        else:
+            file_name = fig.axes[0].get_title()
+        fig.savefig(self._out_svg_dir_path + '/' + file_name + '.svg')
 
     def on_finalize(self):
         pass
@@ -164,6 +169,84 @@ def create_matsize_to_diag_perf_plot() -> plt.Figure:
     return fig
 
 
+def interpolate(req_x: float, vx: numpy.array, vy: numpy.array) -> float:
+    '''
+    req_x: requested x
+    '''
+    # print(f'req_x={req_x}')
+    assert len(vx) == len(vy)
+    xl = vx[0]  # left
+    yl = vy[0]
+    xr = vx[-1]  # right
+    yr = vy[-1]
+    for index in range(len(vx)):
+        xx = vx[index]
+        yy = vy[index]
+        # print(f'xx={xx}, xl={xl}, xr={xr}')
+        if xx < req_x and xx > xl:
+            xl = xx
+            yl = yy
+        if xx > req_x and xx < xr:
+            xr = xx
+            yr = yy
+    # print(f'xl={xl}, xr= {xr}')
+    if xl > req_x or xr < req_x:
+        raise IndexError(f'failed to find the surrounding elements for x = {req_x}')
+    ratio = (req_x - xl) / (xr - xl)
+    req_y = yl + (yr - yl) * ratio
+    # print(f'ratio={ratio}, yl= {yl}, yr= {yr}, req_y= {req_y}')
+    return req_y
+
+
+def create_matsize_to_diag_gpu_gain_plot() -> plt.Figure:
+    # config	num_cores	num_gpu	matrix_size	cpu(s)	gpu_mem_stage1(MiB)	gpu_mem_stage2(MiB)
+    table = numpy.genfromtxt(Path('measurements/fr.univ-rennes1.ipr.physix/diag_bench.tsv'), dtype=("|U10", int, int, int, float, float), names=True, delimiter='\t', usecols=(0, 1, 2, 3, 4))
+    print(table.dtype.names)
+
+    # #measurement_time	host_fqdn	cpu_id	num_cpu	compiler_id	blas_id	max_cores	matrix_size	num_loops	with_eigen_vectors	duration	cpu_time
+    table2 = numpy.genfromtxt(Path('../madiag2/measurements/alambix98.ipr.univ-rennes.fr/runbench.tsv'), dtype=("|U30", "|U30", "|U30", int, "|U30", "|U30", int, int, int, bool, float, float), names=True, delimiter='\t', usecols=(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11))
+    print(table2.dtype.names)
+
+    fig, ax = plt.subplots()
+    gpu_table = table[table['config'] == 'magma']
+
+    # print(table2)
+    cpu2_table = table2[table2['blas_id'] == 'mkl-2023.0.0']
+    cpu2_table = cpu2_table[cpu2_table['max_cores'] == 48]
+    cpu2_table = cpu2_table[cpu2_table['with_eigen_vectors'] == True]  # noqa
+    print(cpu2_table)
+
+    gpu_matrix_sizes = gpu_table['matrix_size']
+    gpu_dsyevd_durations = gpu_table['cpus']
+    cpu_matrix_size = cpu2_table['matrix_size']
+    cpu_dsyevd_durations = cpu2_table['duration'] / cpu2_table['num_loops']
+
+    mat_sizes = []
+    gpu_gains = []   # numpy.zeros(shape=(len(gpu_matrix_sizes),))
+    print(gpu_gains)
+    for gpu_measur_index in range(len(gpu_matrix_sizes)):
+        mat_size = gpu_matrix_sizes[gpu_measur_index]
+        if mat_size > 20000:  # ignore matrix sizes above 20000 (request by flique)
+            continue  
+        gpu_dsyevd_duration = gpu_dsyevd_durations[gpu_measur_index]
+        try:
+            cpu_dsyevd_duration = interpolate(mat_size, cpu_matrix_size, cpu_dsyevd_durations)
+        except IndexError:
+            continue
+        mat_sizes.append(mat_size)
+        gpu_gains.append(cpu_dsyevd_duration / gpu_dsyevd_duration)
+
+    # ax.plot(mat_sizes, gpu_gains, 'o-'), label='gpu nvidia P100 speedup')
+    ax.plot(mat_sizes, gpu_gains, 'o-')
+    plt.xlabel('n: matrix size (n*n matrix)')
+    plt.ylabel('computational speedup')
+    # plt.ylabel('dsyevd computation speedup\ncompared to dual cpu intel xeon 6248r')
+    # plt.title('gpu dsyevd speedup over cpu')
+    fig.file_name = 'gpu-speedup'
+    # ax.legend()
+    return fig
+
+
 if __name__ == '__main__':
     figure_handler = SvgFigureHandler(out_svg_dir_path='./graphics')
     # figure_handler = ScreenFigureHandler()
@@ -171,6 +254,6 @@ if __name__ == '__main__':
     # for fig in figures:
     #     figure_handler.on_figure_ended(fig)
 
-    fig = create_matsize_to_diag_perf_plot()
+    fig = create_matsize_to_diag_gpu_gain_plot()
     figure_handler.on_figure_ended(fig)
     figure_handler.on_finalize()
